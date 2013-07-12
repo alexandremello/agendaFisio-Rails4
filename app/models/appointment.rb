9f22a2ca247e18726d14ef14bd6fc8007be07b2b
solfier <alexandre.rmello@gmail.com>
2013-07-12 18:31:37 -0300

    add some appointment model tests

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ActiveRecord::Base
-	belongs_to :patient
+	belongs_to :patient 
 
+	validates_presence_of :start, :end, :title, :patient_id
+
+	after_create :set_title
+	before_save :set_end
+
+	def set_end
+		self.end.DateTime.now.advance(minutes: 45)
+	end
+
+	def set_title
+		self.title = 'Consulta'
+	end
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -6,7 +6,43 @@ describe Appointment do
 
 		context 'start' do
 			it 'its invalid with nil start' do
-				FactoryGirl.create(:appointment, start: nil).should_not be_valid
+				FactoryGirl.build(:appointment, start: nil).should_not be_valid
+			end
+
+			it 'its invalid with blank start' do
+				FactoryGirl.build(:appointment, start: '').should_not be_valid
+			end
+		end
+
+		context 'end' do
+			it 'its invalid with nil end' do
+				FactoryGirl.build(:appointment, end: nil).should_not be_valid
+			end
+
+			it 'its invalid with blank end' do
+				FactoryGirl.build(:appointment, end: '').should_not be_valid
+			end
+		end
+
+		context 'title' do
+			it 'its invalid with nil title' do
+				FactoryGirl.build(:appointment, title: nil).should_not be_valid
+			end
+
+			it 'its invalid with blank title' do
+				FactoryGirl.build(:appointment, title: '').should_not be_valid
+			end
+		end
+
+		context 'patient_id' do
+
+		end
+	end
+
+	context 'hooks' do
+		context 'after_create' do
+			it 'Title should be Consulta' do
+				FactoryGirl.build(:appointment).title.should == 'Consulta'
 			end
 		end
 	end
